83d163565546276293de5c0aed629dab8801f934
Rukndf <lucca.cezar@edu.pucrs.br>
2020-08-17 17:30:54 -0300

    table

---
+++ src/app/shared/listagem/listagem.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ListagemComponent } from './listagem.component';
+
+@NgModule({
+  declarations: [ListagemComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ListagemModule { }
