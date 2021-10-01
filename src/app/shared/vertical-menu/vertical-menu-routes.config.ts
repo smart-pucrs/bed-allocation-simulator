372327dd6f104b95df1317276effcd6b6de8043e
Debora Engelmann <debo.c.e.3@gmail.com>
2021-10-01 20:46:41 +0200

    add optimiser menu and firebase

--- src/app/shared/vertical-menu/vertical-menu-routes.config.ts
+++ src/app/shared/vertical-menu/vertical-menu-routes.config.ts
@@ -5,6 +5,7 @@ export const ROUTES: RouteInfo[] = [
   { path: '/page/dashboard', title: 'Painel de Controle', icon: 'fa fa-home', class: '', badge: '', badgeClass: 'badge badge-pill badge-danger float-right mr-1 mt-1', isExternalLink: false, submenu: [] },
   { path: '/page/situacao-leitos', title: 'Situação dos Leitos', icon: 'fa fa-bed', class: '', badge: '', badgeClass: 'badge badge-pill badge-danger float-right mr-1 mt-1', isExternalLink: false, submenu: [] },
   { path: '/page/alocacao-leitos', title: 'Alocação de Leitos', icon: 'fa fa-clipboard', class: '', badge: '', badgeClass: 'badge badge-pill badge-danger float-right mr-1 mt-1', isExternalLink: false, submenu: [] },
+  { path: '/page/optimised', title: 'Sugestão de alocação', icon: 'fa fa-file-text-o', class: '', badge: '', badgeClass: 'badge badge-pill badge-danger float-right mr-1 mt-1', isExternalLink: false, submenu: [] },
   { path: '/page/pacientes-internados', title: 'Pacientes Internados', icon: 'fa fa-users', class: '', badge: '', badgeClass: 'badge badge-pill badge-danger float-right mr-1 mt-1', isExternalLink: false, submenu: [] },
   {
     path: '', title: 'Cadastros', icon: 'ft-edit', class: 'has-sub', badge: '', badgeClass: '', isExternalLink: false,
